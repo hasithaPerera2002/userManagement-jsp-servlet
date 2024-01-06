1e9f24f7d60e5626d3235744d0bbe9ef08d0d74d
hasithaPerera2002 <hasithaperera2017@gmail.com>
2024-01-06 12:29:38 +0530

    anew

--- src/main/java/util/DbUtil.java
+++ src/main/java/util/DbUtil.java
@@ -24,7 +24,7 @@ public class DbUtil {
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             String unicode="useSSL=false&autoReconnect=true&useUnicode=yes&characterEncoding=UTF-8";
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/userdb?"+unicode, "root", "");
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/userdb?"+unicode, "root", "1234");
         }catch(Exception ex){
             System.out.println(ex.getMessage());
             System.out.println("couldn't connect!");
